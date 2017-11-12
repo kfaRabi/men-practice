ac73de55e825d33d893b01f7853d8a5df71e235a
Kazi Fozle Azim Rabi <fozleazimrabi2@gmail.com>
2017-11-12 06:10:35 +0600

    install body-parser to access req.body and handle form submission and serverside form validation inside post:/ideas route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -20,8 +21,15 @@ mongoose.connect('mongodb://localhost/waidea', {
 
 const PORT = 8080;
 
+// middlewares
+// handlebars middleware
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.listen(PORT, () => lout(`Server Started On Port: ${PORT}`));
 
@@ -41,4 +49,29 @@ app.get('/about', (req, res) => {
 // add an idea form
 app.get('/ideas/add', (req, res) => {
 	res.render('ideas/add');
+});
+// handle form submission
+app.post('/ideas', (req, res) => {
+	let errors = [];
+	let title, details;
+	if(!req.body.title){
+		errors.push({ text: "Please Enter A Title"});
+	}
+	else {
+		title = req.body.title;
+	}
+	if(!req.body.details){
+		errors.push({ text: "Please Add Some Details"});
+	}
+	else {
+		details = req.body.details;
+	}
+	if(errors.length > 0) {
+		res.render('ideas/add', {
+			errors, title, details,
+		});
+	}
+	else{
+		res.send("done");
+	}
 });
